7223c612f21f84e3c163ab12d291b88be9880781
David Connelly <david.webguy@gmail.com>
2019-08-13 18:40:56 +0100

    removed test files and code

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 //The main config file
-define('BASE_URL', 'http://localhost/trongate_framework/');
+define('BASE_URL', '');
 define('ENV', 'dev');
 define('DEFAULT_MODULE', 'welcome');
 define('DEFAULT_CONTROLLER', 'Welcome');
--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 //Database settings
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
+define('DB_HOST', '');
+define('DB_USER', '');
 define('DB_PASS', '');
-define('DB_NAME', 'trongate_framework');
+define('DB_NAME', '');
