edac4b6257ef37a05b8ddbe49450cccb57bb3f43
MasterX <davy1582@msn.com>
2019-09-08 16:21:23 +0200

    added https redirect

--- modules/security/controllers/Security.php
+++ modules/security/controllers/Security.php
@@ -25,5 +25,14 @@ class Security extends Trongate {
         }
         return $randomString;
     }
+   
+	function _redirect_https(){
+		if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+			$location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+			header('HTTP/1.1 301 Moved Permanently');
+			header('Location: ' . $location);
+			exit;
+		}		
+	}    
 
 }
