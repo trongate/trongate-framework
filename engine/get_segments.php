a8e63710edb3256c64ceffe2327cfed533339ba7
spence01 <adamspencer89@hotmail.com>
2020-12-13 00:49:24 +0000

    Add files via upload
    A) Added the ability to pass multiple (infinitive) vars to methods via the URL. 
    i.e  my_site/mycontroller/mymethod/f/u/b/a/r will now pass to  controller/method/{param1}/{param2}/{param3}/{param4}/{param_5}
    
    B) Added the ability to assign a route with a wild card '*' - This allows vars to be dynamically passed through a routed address. 
    
    for example  'my-route' => 'controller/method*',
    
    url of my-route/f/u/b/a/r will now pass the params to the relevant controller/method.

--- engine/get_segments.php
+++ engine/get_segments.php
@@ -1,7 +1,37 @@
 <?php
-function get_segments($ignore_custom_routes=NULL) {
+function get_segments($ignore_custom_routes=NULL,$route_bypass=NULL) {
 
-    //figure out how many segments needs to be ditched
+    $num_segments_to_ditch = get_num_segments_to_ditch();
+
+    $assumed_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] .  $_SERVER['REQUEST_URI'];
+
+    if (!isset($ignore_custom_routes)) {
+        $assumed_url = attempt_add_custom_routes($assumed_url);
+    }
+
+    $data['assumed_url'] = $assumed_url;
+
+    $segments = get_remaining_segments($assumed_url,$num_segments_to_ditch);
+    $data['segments'] = array_values($segments);     
+    return $data;
+}
+
+function get_remaining_segments($assumed_url,$num_segments_to_ditch){    
+
+    $assumed_url = str_replace('://', '', $assumed_url);
+    $assumed_url = rtrim($assumed_url, '/');
+
+    $segments = explode('/', $assumed_url);    
+
+    for ($i=0; $i < $num_segments_to_ditch; $i++) { 
+        unset($segments[$i]);
+    }
+
+    return $segments;
+}
+
+function get_num_segments_to_ditch(){
+     //figure out how many segments needs to be ditched
     $psuedo_url = str_replace('://', '', BASE_URL);
     $psuedo_url = rtrim($psuedo_url, '/');
     $bits = explode('/', $psuedo_url);
@@ -13,40 +43,54 @@ function get_segments($ignore_custom_routes=NULL) {
         $num_segments_to_ditch = 0;
     }
 
-    $assumed_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] .  $_SERVER['REQUEST_URI'];
-
-    if (!isset($ignore_custom_routes)) {
-        $assumed_url = attempt_add_custom_routes($assumed_url);
-    }
-
-    $data['assumed_url'] = $assumed_url;
-
-    $assumed_url = str_replace('://', '', $assumed_url);
-    $assumed_url = rtrim($assumed_url, '/');
-
-    $segments = explode('/', $assumed_url);
-
-    for ($i=0; $i < $num_segments_to_ditch; $i++) { 
-        unset($segments[$i]);
-    }
-
-    $data['segments'] = array_values($segments); 
-    return $data;
+    return $num_segments_to_ditch;
 }
 
 function attempt_add_custom_routes($target_url) {
     //takes a nice URL and returns the assumed_url
+    //echo $target_url; die;
+    
+    // check to see if the route allows values to be passed.
+    // if it does then check if the request url has additional segments, if so append to the target_url
 
     $target_segment = str_replace(BASE_URL, '', $target_url);
+    //echo $target_segment; die;
 
+    foreach (CUSTOM_ROUTES as $key => $value) {  
 
-    foreach (CUSTOM_ROUTES as $key => $value) {
+        // check for a wildcard, set value & strip target_segment
+        if (strpos($value, '*') !== false) {
+           $value_wild = str_replace('*','', $value);
+           $segments = explode('/',$target_segment); 
+           $first_segment = $segments[0];
+        }      
 
-        if ($key == $target_segment) {
-            $target_url = str_replace($key, $value, $target_url);
-        }
-    }
+        // check for segment match / match with wild card    
+        if ($key == $target_segment || isset($value_wild)) {    
 
+            if(isset($value_wild)){ 
+
+                $num_segments_to_ditch = get_num_segments_to_ditch();                
+                $additional_segments = get_remaining_segments($target_segment,$num_segments_to_ditch);
+                                             
+                // prepare segments as string and append
+                $segment_string = "";
+                foreach($additional_segments as $segment){
+                    $segment_string .= "/".$segment;
+                }
+
+                $segment_string = rtrim($segment_string, '/');               
+                $target_url = str_replace($key, $value_wild, $target_url);
+                $target_url = $target_url.'/'.$segment_string;
+                $target_url = rtrim($target_url, '/');
+                unset($value_wild);                      
+                
+            } else {
+                $target_url = str_replace($key, $value, $target_url);
+            }
+        }   
+    }    
+   
     return $target_url;
 }
 
@@ -62,6 +106,7 @@ function attempt_return_nice_url($target_url) {
         }
 
     }
+
 
     return $target_url;
 }
