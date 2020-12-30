3cc3138e9a721840a69b16935e5afd7e6c85f093
MasterX <davy1582@msn.com>
2020-12-30 20:16:34 +0100

    Removed unnessasary function
    Removed unnessasary function, accedently added in previous commit.

--- modules/security/controllers/Security.php
+++ modules/security/controllers/Security.php
@@ -64,13 +64,4 @@ class Security extends Trongate {
         }
     }
 
-    function _redirect_https(){
-        if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
-            $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-            header('HTTP/1.1 301 Moved Permanently');
-            header('Location: ' . $location);
-            exit;
-        } 
-    }    
-
 }
