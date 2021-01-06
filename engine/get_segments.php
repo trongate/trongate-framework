ed2e401c061cd58d92a8e15f7b4e7a907980c97b
spence01 <adamspencer89@hotmail.com>
2021-01-06 14:52:28 +0000

    Add files via upload

--- engine/get_segments.php
+++ engine/get_segments.php
@@ -19,7 +19,7 @@ function get_segments($ignore_custom_routes=NULL) {
         $assumed_url = attempt_add_custom_routes($assumed_url);
     }
 
-    $data['assumed_url'] = $assumed_url;
+    $data['assumed_url'] = $assumed_url;   
 
     $assumed_url = str_replace('://', '', $assumed_url);
     $assumed_url = rtrim($assumed_url, '/');
@@ -30,23 +30,46 @@ function get_segments($ignore_custom_routes=NULL) {
         unset($segments[$i]);
     }
 
-    $data['segments'] = array_values($segments); 
+    $data['segments'] = array_values($segments);     
+
     return $data;
 }
 
 function attempt_add_custom_routes($target_url) {
-    //takes a nice URL and returns the assumed_url
-
-    $target_segment = str_replace(BASE_URL, '', $target_url);
-
-
+    //takes a nice URL and returns the assumed_url     
+    
+    $target_segments = str_replace(BASE_URL, '', $target_url);
+    $target_segments = explode('/', $target_segments); 
+    if(count($target_segments) > 20)
+        return $target_url;
+    $first_target_segment = $target_segments[0];    
+    
     foreach (CUSTOM_ROUTES as $key => $value) {
-
-        if ($key == $target_segment) {
-            $target_url = str_replace($key, $value, $target_url);
-        }
+        $key_segments = explode('/', $key); 
+        // check that first segment matches    
+        if ($key_segments[0] == $first_target_segment) {                     
+            // remove the segments that are different
+            $dif = array_diff($target_segments,$key_segments); 
+            foreach($dif as $dif_key => $dif_value){
+                unset($target_segments[$dif_key]);
+            }             
+            // rebuild target segments only allowing exact match to the route key 
+            $key_count = count($key_segments); 
+            for ($i = 0; $i < $key_count; $i++) {  
+                if(isset($target_segments[$i]))              
+                $matched_target_segments[] = $target_segments[$i];                
+            }
+            
+            $target_string = implode('/', $matched_target_segments);  
+                                     
+            if($target_string == $key) {                                
+                $additional_segmnets = implode('/',$dif);                        
+                $target_url = BASE_URL.$value.$additional_segmnets;
+                break;                
+            } 
+        }              
     }
-
+    
     return $target_url;
 }
 
