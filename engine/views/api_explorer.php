fbbcd91b6959511db9a34a6413535a21d91186a3
David Connelly <david.webguy@gmail.com>
2021-08-25 16:16:17 +0100

    Replaced all instances of API Explorer with API Manager

--- engine/views/api_explorer.php
+++ engine/views/api_explorer.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>API Explorer</title>
+    <title>API Manager</title>
     <meta name="description" content="">
     <meta name="author" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
