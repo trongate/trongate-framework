63f1e378df35e1c56530f3ab205c4fccb53647e7
David Connelly <david.webguy@gmail.com>
2021-11-07 12:53:46 +0000

    Added some section tags onto the default welcome view file.  No big deal.

--- modules/welcome/views/welcome.php
+++ modules/welcome/views/welcome.php
@@ -5,13 +5,13 @@
     <title>Trongate</title>
 </head>
 <body>
-
+<section>
     <h1>Congratulations</h1>
     <h2>It Totally Works!</h2>
     <p>This page is being generated dynamically by the Trongate Framework.</p>
     <p>To edit this page, go to: modules/welcome/views/welcome.php</p>
     <p>To explore the documentation go to <a href="http://www.trongate.io/documentation">http://www.trongate.io/documentation</a></p>
-    
+</section>    
     <style>
         body {
             font-size: 2em;
