b1eeafe4c639937094819219e2900361766785c3
Simon Field <sfield66@gmail.com>
2022-10-05 22:23:42 +1100

    fix get method

--- engine/Model.php
+++ engine/Model.php
@@ -29,29 +29,29 @@ class Model {
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         );
 
-        try{
+        try {
             $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
-        } catch(PDOException $e){
+        } catch (PDOException $e) {
             $this->error = $e->getMessage();
-            echo $this->error; die();
+            echo $this->error;
+            die();
         }
-
     }
 
     private function get_param_type($value) {
 
-        switch(true){
+        switch (true) {
             case is_int($value):
-            $type = PDO::PARAM_INT;
-            break;
-        case is_bool($value):
-            $type = PDO::PARAM_BOOL;
-            break;
-        case is_null($value):
-            $type = PDO::PARAM_NULL;
-            break;
-        default:
-            $type = PDO::PARAM_STR;
+                $type = PDO::PARAM_INT;
+                break;
+            case is_bool($value):
+                $type = PDO::PARAM_BOOL;
+                break;
+            case is_null($value):
+                $type = PDO::PARAM_NULL;
+                break;
+            default:
+                $type = PDO::PARAM_STR;
         }
 
         return $type;
@@ -71,9 +71,7 @@ class Model {
             }
 
             return $this->stmt->execute();
-
         }
-
     }
 
     private function get_table_from_url() {
@@ -83,8 +81,8 @@ class Model {
     private function correct_tablename($target_tbl) {
         $bits = explode('-', $target_tbl);
         $num_bits = count($bits);
-        if ($num_bits>1) {
-            $target_tbl = $bits[$num_bits-1];
+        if ($num_bits > 1) {
+            $target_tbl = $bits[$num_bits - 1];
         }
 
         return $target_tbl;
@@ -94,14 +92,25 @@ class Model {
 
         if ((is_numeric($limit)) && (is_numeric($offset))) {
             $limit_results = true;
-            $sql.= " LIMIT $offset, $limit";
+            $sql .= " LIMIT $offset, $limit";
         }
 
         return $sql;
-
     }
 
-    public function get($order_by=NULL, $target_tbl=NULL, $limit=NULL, $offset=NULL) {
+    protected function _get_all_tables() {
+        $tables = [];
+        $sql = 'show tables';
+        $column_name = 'Tables_in_' . DATABASE;
+        $rows = $this->query($sql, 'array');
+        foreach ($rows as $row) {
+            $tables[] = $row[$column_name];
+        }
+
+        return $tables;
+    }
+
+    public function get($order_by = NULL, $target_tbl = NULL, $limit = NULL, $offset = NULL) {
 
         $limit_results = false;
 
@@ -112,6 +121,28 @@ class Model {
         if (!isset($target_tbl)) {
             $target_tbl = $this->get_table_from_url();
         }
+
+        $all_tables = $this->_get_all_tables();
+        if (!in_array($target_tbl, $all_tables)) {
+            http_response_code(422);
+            echo 'invalid table name';
+            die();
+        } else {
+            $sql_x = 'DESCRIBE ' . $target_tbl;
+            $result_x = $this->query($sql_x, 'object');
+            $is_valid_order_by = false;
+            foreach ($result_x as $row_x) {
+                if ($row_x->Field == $order_by) {
+                    $is_valid_order_by = true;
+                }
+            }
+
+            if ($is_valid_order_by == false) {
+                echo 'invalid order by value';
+                die();
+            }
+        }
+
 
         $sql = "SELECT * FROM $target_tbl order by $order_by";
 
@@ -141,10 +172,9 @@ class Model {
         $stmt->execute();
         $query = $stmt->fetchAll(PDO::FETCH_OBJ);
         return $query;
-
     }
 
-    public function get_where_custom($column, $value, $operator='=', $order_by='id', $target_tbl=NULL, $limit=NULL, $offset=NULL) {
+    public function get_where_custom($column, $value, $operator = '=', $order_by = 'id', $target_tbl = NULL, $limit = NULL, $offset = NULL) {
 
         if (!isset($target_tbl)) {
             $target_tbl = $this->get_table_from_url();
@@ -166,12 +196,11 @@ class Model {
 
             $operator = strtoupper($operator);
             if (($operator == 'LIKE') || ($operator == 'NOT LIKE')) {
-                $value = '%'.$value.'%';
+                $value = '%' . $value . '%';
                 $data[$column] = $value;
             }
 
             $query_to_execute = $this->show_query($sql, $data, $this->query_caveat);
-
         }
 
         $result = $this->prepare_and_execute($sql, $data);
@@ -180,11 +209,10 @@ class Model {
             $items = $this->stmt->fetchAll(PDO::FETCH_OBJ);
             return $items;
         }
-
     }
 
     //fetch a single record
-    public function get_where($id, $target_tbl=NULL) {
+    public function get_where($id, $target_tbl = NULL) {
 
         $data['id'] = (int) $id;
 
@@ -204,11 +232,10 @@ class Model {
             $item = $this->stmt->fetch(PDO::FETCH_OBJ);
             return $item;
         }
-
     }
 
     //fetch a single record (alternative version)
-    public function get_one_where($column, $value, $target_tbl=NULL) {
+    public function get_one_where($column, $value, $target_tbl = NULL) {
         $data[$column] = $value;
 
         if (!isset($target_tbl)) {
@@ -229,21 +256,21 @@ class Model {
         }
     }
 
-    public function get_many_where($column, $value, $target_tbl=NULL) {
+    public function get_many_where($column, $value, $target_tbl = NULL) {
 
         if (!isset($target_tbl)) {
             $target_tbl = $this->get_table_from_url();
         }
 
         $data[$column] = $value;
-        $sql = 'select * from '.$target_tbl.' where '.$column.' = :'.$column;
+        $sql = 'select * from ' . $target_tbl . ' where ' . $column . ' = :' . $column;
 
         $query = $this->query_bind($sql, $data, 'object');
 
         return $query;
     }
 
-    public function count($target_tbl=NULL) {
+    public function count($target_tbl = NULL) {
         //return number of rows on a table
 
         if (!isset($target_tbl)) {
@@ -263,10 +290,9 @@ class Model {
             $obj = $this->stmt->fetch(PDO::FETCH_OBJ);
             return $obj->total;
         }
-
     }
 
-    public function count_where($column, $value, $operator='=', $order_by='id', $target_tbl=NULL, $limit=NULL, $offset=NULL) {
+    public function count_where($column, $value, $operator = '=', $order_by = 'id', $target_tbl = NULL, $limit = NULL, $offset = NULL) {
         //return number of rows on table (with query customisation)
 
         $query = $this->get_where_custom($column, $value, $operator, $order_by, $target_tbl, $limit, $offset);
@@ -274,7 +300,7 @@ class Model {
         return $num_rows;
     }
 
-    public function count_rows($column, $value, $target_tbl=NULL) {
+    public function count_rows($column, $value, $target_tbl = NULL) {
         //simplified version of count_where (accepts one condition)
 
         if (!isset($target_tbl)) {
@@ -282,7 +308,7 @@ class Model {
         }
 
         $data[$column] = $value;
-        $sql = 'SELECT COUNT(id) as total from '.$target_tbl.' where '.$column.' = :'.$column;
+        $sql = 'SELECT COUNT(id) as total from ' . $target_tbl . ' where ' . $column . ' = :' . $column;
 
         if ($this->debug == true) {
             $query_to_execute = $this->show_query($sql, $data);
@@ -294,10 +320,9 @@ class Model {
             $obj = $this->stmt->fetch(PDO::FETCH_OBJ);
             return $obj->total;
         }
-
     }
 
-    public function get_max($target_tbl=NULL) {
+    public function get_max($target_tbl = NULL) {
 
         if (!isset($target_tbl)) {
             $target_tbl = $this->get_table_from_url();
@@ -317,10 +342,9 @@ class Model {
             $max_id = $assoc['max_id'];
             return $max_id;
         }
-
     }
 
-    public function show_query($query, $data, $caveat=NULL) {
+    public function show_query($query, $data, $caveat = NULL) {
         $keys = array();
         $values = $data;
         $named_params = true;
@@ -329,7 +353,7 @@ class Model {
         foreach ($data as $key => $value) {
 
             if (is_string($key)) {
-                $keys[] = '/:'.$key.'/';
+                $keys[] = '/:' . $key . '/';
             } else {
                 $keys[] = '/[?]/';
                 $named_params = false;
@@ -349,64 +373,62 @@ class Model {
             $query = preg_replace($keys, $values, $query);
         } else {
 
-            $query = $query.' ';
+            $query = $query . ' ';
             $bits = explode(' ? ', $query);
 
             $query = '';
-            for ($i=0; $i < count($bits); $i++) {
-                $query.= $bits[$i];
+            for ($i = 0; $i < count($bits); $i++) {
+                $query .= $bits[$i];
 
                 if (isset($values[$i])) {
-                    $query.= ' '.$values[$i].' ';
+                    $query .= ' ' . $values[$i] . ' ';
                 }
-
             }
-
         }
 
         if (!isset($caveat)) {
             $caveat_info = '';
         } else {
 
-            $caveat_info = '<br><hr><div style="font-size: 0.8em;"><b>PLEASE NOTE:</b> '.$caveat;
-            $caveat_info.= ' PDO currently has no means of displaying previous query executed.</div>';
+            $caveat_info = '<br><hr><div style="font-size: 0.8em;"><b>PLEASE NOTE:</b> ' . $caveat;
+            $caveat_info .= ' PDO currently has no means of displaying previous query executed.</div>';
         }
 
         echo '<div class="tg-rprt"><b>QUERY TO BE EXECUTED:</b><br><br>  -> ';
-        echo $query.$caveat_info.'</div>';
-        ?>
+        echo $query . $caveat_info . '</div>';
+?>
 
-<style>
-.tg-rprt {
-color: #383623;
-background-color: #efe79e;
-font-family: "Lucida Console", Monaco, monospace;
-padding: 1em;
-border: 1px #383623 solid;
-clear: both !important;
-margin: 1em 0;
-}
-</style>
+        <style>
+            .tg-rprt {
+                color: #383623;
+                background-color: #efe79e;
+                font-family: "Lucida Console", Monaco, monospace;
+                padding: 1em;
+                border: 1px #383623 solid;
+                clear: both !important;
+                margin: 1em 0;
+            }
+        </style>
 
-    <?php
+<?php
     }
 
-    public function insert($data, $target_tbl=NULL) {
+    public function insert($data, $target_tbl = NULL) {
 
         if (!isset($target_tbl)) {
             $target_tbl = $this->get_table_from_url();
         }
 
-        $sql = 'INSERT INTO `'.$target_tbl.'` (';
-        $sql.= '`'.implode("`, `", array_keys($data)).'`)';
-        $sql.= ' VALUES (';
+        $sql = 'INSERT INTO `' . $target_tbl . '` (';
+        $sql .= '`' . implode("`, `", array_keys($data)) . '`)';
+        $sql .= ' VALUES (';
 
         foreach ($data as $key => $value) {
-            $sql.=':'.$key.', ';
+            $sql .= ':' . $key . ', ';
         }
 
         $sql = rtrim($sql, ', ');
-        $sql.=')';
+        $sql .= ')';
 
         if ($this->debug == true) {
             $query_to_execute = $this->show_query($sql, $data, $this->query_caveat);
@@ -417,7 +439,7 @@ margin: 1em 0;
         return $id;
     }
 
-    public function update($update_id, $data, $target_tbl=NULL) {
+    public function update($update_id, $data, $target_tbl = NULL) {
 
         if (!isset($target_tbl)) {
             $target_tbl = $this->get_table_from_url();
@@ -426,11 +448,11 @@ margin: 1em 0;
         $sql = "UPDATE `$target_tbl` SET ";
 
         foreach ($data as $key => $value) {
-            $sql.= "`$key` = :$key, ";
+            $sql .= "`$key` = :$key, ";
         }
 
         $sql = rtrim($sql, ', ');
-        $sql.= " WHERE `$target_tbl`.`id` = :id";
+        $sql .= " WHERE `$target_tbl`.`id` = :id";
 
         $data['id'] = (int) $update_id;
         $data = $data;
@@ -440,10 +462,9 @@ margin: 1em 0;
         }
 
         $this->prepare_and_execute($sql, $data);
-
     }
 
-    public function delete($id, $target_tbl=NULL) {
+    public function delete($id, $target_tbl = NULL) {
 
         if (!isset($target_tbl)) {
             $target_tbl = $this->get_table_from_url();
@@ -457,10 +478,9 @@ margin: 1em 0;
         }
 
         $this->prepare_and_execute($sql, $data);
-
     }
 
-    public function query($sql, $return_type=false) {
+    public function query($sql, $return_type = false) {
 
         //WARNING: very high risk of SQL injection - use with caution!
         $data = [];
@@ -480,12 +500,10 @@ margin: 1em 0;
             }
 
             return $query;
-
         }
-
     }
 
-    public function query_bind($sql, $data, $return_type=false) {
+    public function query_bind($sql, $data, $return_type = false) {
 
         if ($this->debug == true) {
             $query_to_execute = $this->show_query($sql, $data, $this->query_caveat);
@@ -502,9 +520,7 @@ margin: 1em 0;
             }
 
             return $query;
-
         }
-
     }
 
     public function insert_batch($table, array $records) {
@@ -539,5 +555,4 @@ margin: 1em 0;
             echo 'Feature disabled, since not on \'dev\' mode.';
         }
     }
-
 }
