6d93faf50ec559d31e562a33a618a9b39eecb1c5
David Connelly <david.webguy@gmail.com>
2023-02-01 16:02:28 +0000

    updated default homepage message

--- modules/welcome/views/welcome.php
+++ modules/welcome/views/welcome.php
@@ -6,11 +6,13 @@
 </head>
 <body>
 <section>
-    <h1>Congratulations</h1>
-    <h2>It Totally Works!</h2>
-    <p>This page is being generated dynamically by the Trongate Framework.</p>
-    <p>To edit this page, go to: modules/welcome/views/welcome.php</p>
-    <p>To explore the documentation go to <a href="http://www.trongate.io/documentation">http://www.trongate.io/documentation</a></p>
+    <h1>It Totally Works!</h1>
+    <p>Welcome to Trongate - the revolutionary new framework for developers who love pure PHP.</p>
+
+    <p>To explore the documentation go to <a href="https://trongate.io/docs" target="_blank">https://trongate.io/docs</a></p>
+
+    <p><span class="blink"><a href="https://github.com/trongate/trongate-framework" target="_blank">Please give Trongate a star on GitHub</a>.</span> If Trongate becomes a top ten PHP framework, it'll be the most electrifying event in the history of web development!  Join the revolution!  Be part of something amazing.  Together, we <i><span style="color: white">SHALL</span></i> make PHP great again!</p>
+
 </section>    
     <style>
         body {
@@ -19,6 +21,15 @@
             color: #ddd;
             text-align: center;
             font-family: "Lucida Console", Monaco, monospace;
+        }
+
+        section {
+            max-width: 1200px;
+            margin:  0 auto;
+        }
+
+        section p {
+            line-height: 1.3em;
         }
 
         h1 {
@@ -30,6 +41,24 @@
         }
 
         a { color: white; }
+
+        .blink{
+            text-decoration: blink;
+            -webkit-animation-name: blinker;
+            -webkit-animation-duration: 0.5s;
+            -webkit-animation-iteration-count:infinite;
+            -webkit-animation-timing-function:ease-in-out;
+            -webkit-animation-direction: alternate;
+        }
+
+        @-webkit-keyframes blinker {
+          from {opacity: 1.0;}
+          to {opacity: 0.0;}
+        }
+
+        .blink a:hover {
+            color: yellow;
+        }
 
     </style>
 </body>
