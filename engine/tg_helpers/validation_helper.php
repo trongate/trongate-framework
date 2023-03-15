9007578c8bf944ff99759599097bd30e08293a4e
David Connelly <david.webguy@gmail.com>
2023-03-15 08:23:42 +0000

    Correction to valid_email method

--- engine/tg_helpers/validation_helper.php
+++ engine/tg_helpers/validation_helper.php
@@ -402,32 +402,36 @@ class Validation_helper {
     private function valid_email($validation_data) {
         extract($validation_data);
 
-        if ((!filter_var($posted_value, FILTER_VALIDATE_EMAIL)) && ($posted_value !== '')) {
-            $this->form_submission_errors[$key][] = 'The '.$label.' field must contain a valid email address.';
-            return;
-        }
+        if ($posted_value !== '') {
 
-        // Check if the email address contains an @ symbol and a valid domain name
-        $at_pos = strpos($posted_value, '@');
-        if ($at_pos === false || $at_pos === 0) {
-            $this->form_submission_errors[$key][] = 'The '.$label.' is not properly formatted.';
-            return;
-        }
-
-        // Make sure the email address is not too long
-        if (strlen($posted_value) > 254) {
-            $this->form_submission_errors[$key][] = 'The '.$label.' is too long.';
-            return;
-        }
-
-        // Check if the internet is available
-        if($sock = @fsockopen('www.google.com', 80)) {
-           fclose($sock);
-            $domain_name = substr($posted_value, $at_pos + 1);
-            if (!checkdnsrr($domain_name, 'MX')) {
-                $this->form_submission_errors[$key][] = 'The '.$label.' field contains an invalid domain name';
+            if (!filter_var($posted_value, FILTER_VALIDATE_EMAIL)) {
+                $this->form_submission_errors[$key][] = 'The '.$label.' field must contain a valid email address.';
                 return;
             }
+
+            // Check if the email address contains an @ symbol and a valid domain name
+            $at_pos = strpos($posted_value, '@');
+            if ($at_pos === false || $at_pos === 0) {
+                $this->form_submission_errors[$key][] = 'The '.$label.' is not properly formatted.';
+                return;
+            }
+
+            // Make sure the email address is not too long
+            if (strlen($posted_value) > 254) {
+                $this->form_submission_errors[$key][] = 'The '.$label.' is too long.';
+                return;
+            }
+
+            // Check if the internet is available
+            if($sock = @fsockopen('www.google.com', 80)) {
+               fclose($sock);
+                $domain_name = substr($posted_value, $at_pos + 1);
+                if (!checkdnsrr($domain_name, 'MX')) {
+                    $this->form_submission_errors[$key][] = 'The '.$label.' field contains an invalid domain name';
+                    return;
+                }
+            }
+
         }
 
     }
