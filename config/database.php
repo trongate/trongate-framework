941aa62651d4e89b5a5e9988720c5663dc8f3bd1
Andrew Suares <35380745+Andrewsuares@users.noreply.github.com>
2023-05-21 10:25:58 +0530

    Update database.php
    Quicky change to a SQLite database by changing the params in the database config!

--- config/database.php
+++ config/database.php
@@ -1,6 +1,9 @@
 <?php
+
 //Database settings
 define('HOST', 'localhost');
 define('USER', 'root');
 define('PASSWORD', '');
 define('DATABASE', '');
+define('DB_TYPE', 'mysql'); // mysql, SQLite
+define('DB_SQLITE_FILENAME', 'database.sqlite');
