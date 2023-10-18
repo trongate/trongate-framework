1ebb07cc514b2289105f1d9aca5e8a5c5f85bebc
David Connelly <davidc.webguy@gmail.com>
2023-10-18 22:54:08 +0100

    Added type hinting, return-types and doc blocks to Dynamic_properties class.

--- engine/Dynamic_properties.php
+++ engine/Dynamic_properties.php
@@ -1,11 +1,28 @@
 <?php
+/**
+ * Trait Dynamic_properties - Provides dynamic property handling methods.
+ */
 trait Dynamic_properties {
     private $attributes = [];
     
-    public function __set(string $key, object $value): void {
+
+    /**
+     * Set a dynamic property.
+     *
+     * @param string $key The property key.
+     * @param mixed $value The value to set.
+     * @return void
+     */
+    public function __set(string $key, $value): void {
         $this->attributes[$key] = $value;
     }
 
+    /**
+     * Get a dynamic property.
+     *
+     * @param string $key The property key.
+     * @return mixed The value of the property.
+     */
     public function __get(string $key) {
 
         if ($key === 'model') {
