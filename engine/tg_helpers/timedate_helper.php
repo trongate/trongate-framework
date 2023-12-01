bb5e4c9bb4af063e4a9cbe0eda85fb3b9c5d527c
David Connelly <davidc.webguy@gmail.com>
2023-12-01 18:15:56 +0000

    Added doc blocks, type hinting and return types to timedate_helper.php.  Also added two new functions for converting date and time input values into date objects.

--- engine/tg_helpers/timedate_helper.php
+++ engine/tg_helpers/timedate_helper.php
@@ -1,5 +1,12 @@
 <?php
-function get_default_date_format() {
+/**
+ * Initializes the default date format constant if not already defined.
+ *
+ * If DEFAULT_DATE_FORMAT is not defined, sets it to 'mm/dd/yyyy' (US date format).
+ *
+ * @return void
+ */
+function get_default_date_format(): void {
     if (!defined('DEFAULT_DATE_FORMAT')) {
         define('DEFAULT_DATE_FORMAT', 'mm/dd/yyyy'); // US date format.
     }
@@ -7,7 +14,14 @@ function get_default_date_format() {
     // No need to return DEFAULT_DATE_FORMAT since it's already global.
 }
 
-function get_default_locale_str() {
+/**
+ * Initializes the default locale string constant if not already defined.
+ *
+ * If DEFAULT_LOCALE_STR is not defined, sets it to 'en-US' (US English).
+ *
+ * @return void
+ */
+function get_default_locale_str(): void {
     if (!defined('DEFAULT_LOCALE_STR')) {
         define('DEFAULT_LOCALE_STR', 'en-US'); // US English.
     }
@@ -15,9 +29,17 @@ function get_default_locale_str() {
     // No need to return DEFAULT_LOCALE_STR since it's already global.
 }
 
-function format_date_str($stored_date_str) {
+/**
+ * Formats a date string.
+ *
+ * Accepts a date string ($stored_date_str) expected in 'yyyy-mm-dd' format and formats it
+ * according to the DEFAULT_DATE_FORMAT constant.
+ *
+ * @param string $stored_date_str The date string to be formatted (expected format: 'yyyy-mm-dd').
+ * @return string The formatted date string as per the DEFAULT_DATE_FORMAT or the original string if an error occurs.
+ */
+function format_date_str(string $stored_date_str): string {
     try {
-        // Function to format a date string ($stored_date_str) expected in 'yyyy-mm-dd' format
         get_default_date_format(); // Ensuring DEFAULT_DATE_FORMAT is set
         
         // Constructing DateTime object from the provided date string
@@ -40,9 +62,17 @@ function format_date_str($stored_date_str) {
     }
 }
 
-function format_datetime_str($stored_datetime_str) {
+/**
+ * Formats a date-time string.
+ *
+ * Accepts a date-time string ($stored_datetime_str) expected in 'yyyy-mm-dd HH:ii:ss' format and formats it
+ * according to the DEFAULT_DATE_FORMAT constant.
+ *
+ * @param string $stored_datetime_str The date-time string to be formatted (expected format: 'yyyy-mm-dd HH:ii:ss').
+ * @return string The formatted date-time string as per the DEFAULT_DATE_FORMAT or the original string if an error occurs.
+ */
+function format_datetime_str(string $stored_datetime_str): string {
     try {
-        // Function to format a date-time string ($stored_datetime_str) expected in 'yyyy-mm-dd HH:ii:ss' format
         get_default_date_format(); // Ensuring DEFAULT_DATE_FORMAT is set
         
         // Constructing DateTime object from the provided date-time string
@@ -68,9 +98,17 @@ function format_datetime_str($stored_datetime_str) {
     }
 }
 
-function format_time_str($stored_time_str) {
+/**
+ * Formats a time string.
+ *
+ * Accepts a time string ($stored_time_str) expected in 'HH:ii' format and formats it
+ * according to the 'h:i' or 'HH:ii' format based on the provided time.
+ *
+ * @param string $stored_time_str The time string to be formatted (expected format: 'HH:ii').
+ * @return string The formatted time string as per the 'h:i' or 'HH:ii' format or the original string if an error occurs.
+ */
+function format_time_str(string $stored_time_str): string {
     try {
-        // Function to format a time string ($stored_time_str) expected in 'HH:ii' format
         $time_parts = explode(':', $stored_time_str);
     
         $hours = (int)$time_parts[0];
@@ -91,4 +129,59 @@ function format_time_str($stored_time_str) {
     } catch (Exception $e) {
         return $stored_time_str; // Return the original string in case of error
     }
+}
+
+/**
+ * Converts a string representation of a date or date-time to a DateTime object.
+ * 
+ * Accepts date strings in formats 'dd/mm/yyyy', 'dd-mm-yyyy', 'mm/dd/yyyy', or 'mm-dd-yyyy',
+ * and date-time strings in the format 'mm/dd/yyyy, HH:ii'.
+ * 
+ * @param string $input_str A string representing a date or date-time.
+ * @return DateTime|null Returns a DateTime object if successful, otherwise returns null.
+ */
+function convert_to_date_obj(string $input_str): ?DateTime {
+    get_default_date_format();
+    $possible_formats = ['d/m/Y', 'd-m-Y', 'm/d/Y', 'm-d-Y'];
+    $default_date_format = DEFAULT_DATE_FORMAT;
+
+    // Pattern to match date-time string 'mm/dd/yyyy, HH:ii'
+    $datetime_pattern = '/^(\d{2}[\/-]\d{2}[\/-]\d{4}),\s(\d{2}:\d{2})$/';
+
+    if (preg_match($datetime_pattern, $input_str, $matches)) {
+        $date_time = DateTime::createFromFormat('m/d/Y, H:i', $input_str);
+
+        if ($date_time instanceof DateTime) {
+            return $date_time;
+        }
+    } else {
+        foreach ($possible_formats as $format) {
+            $date_obj = DateTime::createFromFormat($format, $input_str);
+
+            if ($date_obj instanceof DateTime) {
+                return $date_obj;
+            }
+        }
+    }
+
+    return null; // Returning null if date object cannot be created
+}
+
+/**
+ * Converts a string representation of time to a DateTime object.
+ * 
+ * Accepts time strings in the format 'HH:mm'.
+ * 
+ * @param string $input_time_str A string representing time in 'HH:mm' format.
+ * @return DateTime|null Returns a DateTime object if successful, otherwise returns null.
+ */
+function convert_time_str(string $input_time_str): ?DateTime {
+    $time_pattern = '/^([01]?[0-9]|2[0-3]):[0-5][0-9]$/';
+
+    if (preg_match($time_pattern, $input_time_str)) {
+        $time_obj = DateTime::createFromFormat('H:i', $input_time_str);
+        return $time_obj !== false ? $time_obj : null;
+    }
+
+    return null; // Returning null if time object cannot be created
 }
