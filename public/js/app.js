fb7d42d90516daf5b426aa618b41a67117925c81
Dom Sinclair <dom@viewtolearn.co.uk>
2024-06-06 20:18:12 +0100

    Added modalClosed event to the closeModal() functions in admin.js and app.js

--- public/js/app.js
+++ public/js/app.js
@@ -82,6 +82,9 @@ function closeModal() {
     if (overlay) {
       overlay.remove();
     }
+    // Dispatch a custom event indicating modal closure
+    var event = new Event('modalClosed', { bubbles: true, cancelable: true });
+    document.dispatchEvent(event);
   }
 }
 
