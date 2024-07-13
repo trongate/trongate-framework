6f0e382f58bf41bc7d8f9cde057465bb8cd3a400
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2024-07-13 22:30:45 +0200

    Change `language` to `locale` for consistency

--- modules/trongate_localization/setup.sql
+++ modules/trongate_localization/setup.sql
@@ -3,19 +3,18 @@ START TRANSACTION;
 
 CREATE TABLE IF NOT EXISTS `trongate_localization`
 (
-    `id`       int(11) NOT NULL AUTO_INCREMENT,
-    `language` varchar(65) DEFAULT NULL,
-    `key`      varchar(60) DEFAULT NULL,
-    `value`    LONGTEXT    DEFAULT NULL,
-    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `id`         int(11) NOT NULL AUTO_INCREMENT,
+    `locale`     varchar(65) DEFAULT NULL,
+    `key`        varchar(60) DEFAULT NULL,
+    `value`      LONGTEXT    DEFAULT NULL,
+    `created_at` TIMESTAMP   DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
-INSERT INTO `trongate_localization` (`id`, `language`, `key`, `value`)
-VALUES
-    (1, 'da', 'Hello', 'Haløjsa Trongate'),
-    (2, 'en', 'Hello', 'Hello Trongate'),
-    (3, 'da', 'Manage Localizations', 'Administrer oversættelser'),
-    (4, 'en', 'Manage Localizations', 'Manage Localizations');
+INSERT INTO `trongate_localization` (`id`, `locale`, `key`, `value`)
+VALUES (1, 'da_DK', 'Hello', 'Haløjsa Trongate'),
+       (2, 'en_US', 'Hello', 'Hello Trongate'),
+       (3, 'da_DK', 'Manage Localizations', 'Administrer oversættelser'),
+       (4, 'en_US', 'Manage Localizations', 'Manage Localizations');
 COMMIT;
