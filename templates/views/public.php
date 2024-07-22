3482be5ecb20895b4cff3a0d3d4209daef9d0f0d
David Connelly <davidc.webguy@gmail.com>
2024-07-22 18:50:16 +0100

    Added 'base' tag to the HTML templates.  This will allow us to reference internal paths (for example, paths to JavaScript or CSS files) without having to write <?= BASE_URL ?> in the path string.

--- templates/views/public.php
+++ templates/views/public.php
@@ -1,31 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<base href="<?= BASE_URL ?>">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="<?= BASE_URL ?>css/trongate.css">
-	<link rel="stylesheet" href="<?= BASE_URL ?>css/app.css">
-	<!-- don't change anything above here -->
-	<!-- add your own stylesheet below here -->
+	<link rel="stylesheet" href="css/trongate.css">
+	<link rel="stylesheet" href="css/app.css">
 	<title>Public</title>
 </head>
-
 <body>
 	<div class="wrapper">
 		<header>
 			<div id="header-sm">
-				<div id="hamburger" onclick="openSlideNav()">
-					&#9776;
-				</div>
+				<div id="hamburger" onclick="openSlideNav()">&#9776;</div>
 				<div class="logo">
 					<?= anchor(BASE_URL, WEBSITE_NAME) ?>
 				</div>
-				<div><?php
-							echo anchor('account', '<i class="fa fa-user"></i>');
-							echo anchor('logout', '<i class="fa fa-sign-out"></i>');
-							?></div>
+				<div>
+					<?= anchor('account', '<i class="fa fa-user"></i>') ?>
+					<?= anchor('logout', '<i class="fa fa-sign-out"></i>') ?>
+				</div>
 			</div>
 			<div id="header-lg">
 				<div class="logo">
@@ -55,7 +50,6 @@
 		<div id="close-btn" onclick="closeSlideNav()">&times;</div>
 		<ul auto-populate="true"></ul>
 	</div>
-	<script src="<?= BASE_URL  ?>js/app.js"></script>
+	<script src="js/app.js"></script>
 </body>
-
 </html>
