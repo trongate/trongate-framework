bb5698169698cb054826ea5e74f0727dc7c8bd03
David Connelly <davidc.webguy@gmail.com>
2025-02-06 09:49:54 +0000

    Updated _make_sure_allowed method to return mixed type
    - Changed return type hint from string|bool|null to mixed to allow for more flexible return types.
    - Updated docblock to reflect the new return type.

--- modules/trongate_security/controllers/Trongate_security.php
+++ modules/trongate_security/controllers/Trongate_security.php
@@ -6,16 +6,16 @@ class Trongate_security extends Trongate {
      *
      * @param string $scenario The scenario for access control. Default is 'admin panel'.
      * @param array $params (Optional) Additional parameters for more granular control.
-     * @return string|bool|null Returns a Trongate token, a boolean, or null.
+     * @return mixed Returns a Trongate token, a boolean, or null.
      */
-    public function _make_sure_allowed(string $scenario = 'admin panel', array $params = []): string|bool|null {
+    public function _make_sure_allowed(string $scenario = 'admin panel', array $params = []): mixed {
         // Returns either a Trongate token, a boolean, or null.
 
         switch ($scenario) {
-                // case 'members area':
-                //     $this->module('members');
-                //     $token = $this->members->_make_sure_allowed();
-                //     break;
+            // case 'members area':
+            //     $this->module('members');
+            //     $token = $this->members->_make_sure_allowed();
+            //     break;
             default:
                 $this->module('trongate_administrators');
                 $token = $this->trongate_administrators->_make_sure_allowed($scenario, $params);
@@ -24,4 +24,5 @@ class Trongate_security extends Trongate {
 
         return $token;
     }
+
 }
