ab8b2743229aa5e0af5d29132c9674bbed13bf8d
David Connelly <davidc.webguy@gmail.com>
2026-01-20 16:35:19 +0000

    Added 'base' tag and $additional_includes_btm to the public template view file.

--- modules/templates/views/public.php
+++ modules/templates/views/public.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <base href="<?= BASE_URL ?>">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= BASE_URL ?>css/trongate.css">
@@ -11,5 +12,6 @@
     <div class="container">
         <div class="text-center"><?= display($data) ?></div>
     </div>
+<?= $additional_includes_btm ?? '' ?>
 </body>
 </html>
